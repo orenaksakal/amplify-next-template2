779b43016fe181157e746f0be63fdac95e9c8e43
Oren Aksakal <orenaksakal@gmail.com>
2024-09-08 03:22:31 -0600

    dddd

--- app/page.tsx
+++ app/page.tsx
@@ -27,7 +27,7 @@ export default function App() {
 
   function createTodo() {
     client.models.Todo.create({
-      content: window.prompt("Todo content"),
+      content: Math.random() + "I"
     });
   }
 
